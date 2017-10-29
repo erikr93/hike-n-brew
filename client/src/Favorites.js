e1f89962499ca7f0c882e1aa188745dafd6bd4ba
erikr93 <erikr93@gmail.com>
2017-10-29 11:45:09 -0700

    adjusted button

--- client/src/Favorites.js
+++ client/src/Favorites.js
@@ -33,12 +33,6 @@ class Favorites extends Component {
           <p className="address">Address</p>
           <p className="hours">9am-5pm</p>
         </div>
-        <div className="fav well">
-          <img src="https://placeimg.com/100/100/nature" className="img-thumbnail fav-img pull-left" />
-          <h4>Name of place</h4>
-          <p className="address">Address</p>
-          <p className="hours">9am-5pm</p>
-        </div>
       </div>
     )
   }
